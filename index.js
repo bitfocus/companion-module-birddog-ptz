a536377955c0acecb710fff065a5976af6f96049
thedist <dist@dist.dev>
2020-12-05 00:47:54 +0000

    Correct buffer

--- index.js
+++ index.js
@@ -159,7 +159,7 @@ var PRESET = [
 
 instance.prototype.sendVISCACommand = function(payload) {
 	var self = this;
-	var buf = Buffer.from(32);
+	var buf = Buffer.alloc(32);
 
 		// 0x01 0x00 = VISCA Command
 		buf[0] = 0x01;
@@ -187,7 +187,7 @@ instance.prototype.sendVISCACommand = function(payload) {
 
 instance.prototype.sendVISCACommand = function(payload, counter) {
 	var self = this;
-	var buf = Buffer.from(32);
+	var buf = Buffer.alloc(32);
 
 		// 0x01 0x00 = VISCA Command
 		buf[0] = 0x01;
@@ -306,7 +306,7 @@ instance.prototype.sendResponse = function(data) {
 
 instance.prototype.sendControlCommand = function(payload) {
 	var self = this;
-	var buf = Buffer.from(32);
+	var buf = Buffer.alloc(32);
 
 	// 0x01 0x00 = VISCA Command
 	buf[0] = 0x02;
